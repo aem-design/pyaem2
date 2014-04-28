efa15650473154284498ef19df6c9026ae18c50f
Cliffano Subagio <cliffano@gmail.com>
2014-04-28 22:52:54 +1000

    Add create package test with handlers matcher.

--- test/test_packagemanager.py
+++ test/test_packagemanager.py
@@ -1,5 +1,16 @@
+from mock import MagicMock
 import pyaem
+from pyaem import bagofrequests as bag
 import unittest
+
+class HandlersMatcher(object):
+
+    def __init__(self, handler_keys):
+        self.handler_keys = handler_keys
+
+    def __eq__(self, handlers):
+        return handlers.keys() == self.handler_keys
+
 
 class TestPackageManager(unittest.TestCase):
 
@@ -36,6 +47,18 @@ class TestPackageManager(unittest.TestCase):
 
         self.assertEquals(result['status'], 'failure')
         self.assertEquals(result['message'], 'some message')
+
+
+    def test_create_package(self):
+
+        bag.request = MagicMock()
+        packagemanager = pyaem.packagemanager.PackageManager('http://localhost:4502/.cqactions.html')
+        packagemanager.create_package('mygroup', 'mypackage', '1.0-SNAPSHOT', foo='bar')
+        bag.request.assert_called_once_with(
+            'post',
+            'http://localhost:4502/.cqactions.html/crx/packmgr/service/.json/etc/packages/mypackage',
+            {'packageName': 'mypackage', 'cmd': 'create', 'groupName': 'mygroup', '_charset_': 'utf-8', 'packageVersion': '1.0-SNAPSHOT', 'foo': 'bar'},
+            HandlersMatcher([200, 401, 405]))
 
 
 if __name__ == '__main__':

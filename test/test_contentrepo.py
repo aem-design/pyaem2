302ffcd8692ca0d0fc0b68136a776366682f3768
Cliffano Subagio <cliffano@gmail.com>
2014-04-30 08:56:33 +1000

    Add create_path test.

--- test/test_contentrepo.py
+++ test/test_contentrepo.py
@@ -1,5 +1,8 @@
+from mock import MagicMock
 import pyaem
+from pyaem import bagofrequests as bag
 import unittest
+from .util import HandlersMatcher
 
 class TestContentRepo(unittest.TestCase):
 
@@ -13,6 +16,19 @@ class TestContentRepo(unittest.TestCase):
 
         self.assertTrue(401 in contentrepo.handlers)
         self.assertTrue(405 in contentrepo.handlers)
+
+
+    def test_create_path(self):
+
+        bag.request = MagicMock()
+        contentrepo = pyaem.contentrepo.ContentRepo('http://localhost:4502/.cqactions.html')
+        contentrepo.create_path('/content/somepath', foo='bar')
+        bag.request.assert_called_once_with(
+            'post',
+            'http://localhost:4502/.cqactions.html//content/somepath',
+            {'foo': 'bar'},
+            HandlersMatcher([200, 401, 405]))
+
 
 if __name__ == '__main__':
     unittest.main()

8b0300aee6c00eae176edd4862756fc4f7c7baca
Cliffano Subagio <cliffano@gmail.com>
2014-05-11 16:05:57 +1000

    Add docstrings.

--- pyaem/bagofrequests.py
+++ pyaem/bagofrequests.py
@@ -4,7 +4,22 @@ import pycurl
 import urllib
 
 def request(method, url, params, handlers, **kwargs):
+    """Sends HTTP request to a specified URL.
+    Parameters will be appended to URL automatically on HTTP get method.
+    Response code will then be used to determine which handler should process the response.
+    When response code does not match any handler, an exception will be raised.
 
+    :param method: HTTP method (post, delete, get)
+    :type method: str
+    :param url: URL to send HTTP request to
+    :type url: str
+    :param params: Request parameters key-value pairs, use array value to represent multi parameters with the same name
+    :type params: dict
+    :param handlers: Response handlers key-value pairs, keys are response http code, values are callback methods
+    :type handlers: dict
+    :returns:  PyAemResult -- Result of the request containing status, response http code and body, and request info
+    :raises: PyAemException
+    """
     curl = pycurl.Curl()
     body_io = cStringIO.StringIO()
 
@@ -41,7 +56,22 @@ def request(method, url, params, handlers, **kwargs):
 
 
 def download_file(url, params, handlers, **kwargs):
+    """Downloads a file from specified URL, the file will be downloaded to path specified in file kwarg.
+    Parameters will be appended to URL automatically on HTTP get method.
+    Response code will then be used to determine which handler should process the response.
+    When response code does not match any handler, an exception will be raised.
 
+    :param url: URL to send HTTP request to
+    :type url: str
+    :param params: Request parameters key-value pairs, use array value to represent multi parameters with the same name
+    :type params: dict
+    :param handlers: Response handlers key-value pairs, keys are response http code, values are callback methods
+    :type handlers: dict
+    :param kwargs: file (str) -- Location where the downloaded file will be saved to
+    :type kwargs: dict
+    :returns:  PyAemResult -- The result containing status, response http code and body, and request info
+    :raises: PyAemException
+    """
     curl = pycurl.Curl()
     body_io = cStringIO.StringIO()
     url = '{0}?{1}'.format(url, urllib.urlencode(params, True))
@@ -74,7 +104,23 @@ def download_file(url, params, handlers, **kwargs):
 
 
 def upload_file(url, params, handlers, **kwargs):
+    """Uploads a file using the specified URL endpoint,
+    the file to be uploaded must be available at the specified location in file kwarg.
+    Parameters will be appended to URL automatically on HTTP get method.
+    Response code will then be used to determine which handler should process the response.
+    When response code does not match any handler, an exception will be raised.
 
+    :param url: URL to send HTTP request to
+    :type url: str
+    :param params: Request parameters key-value pairs, use array value to represent multi parameters with the same name
+    :type params: dict
+    :param handlers: Response handlers key-value pairs, keys are response http code, values are callback methods
+    :type handlers: dict
+    :param kwargs: file (str) -- Location of the file to be uploaded
+    :type kwargs: dict
+    :returns:  PyAemResult -- The result containing status, response http code and body, and request info
+    :raises: PyAemException
+    """
     curl = pycurl.Curl()
     body_io = cStringIO.StringIO()
     _params = []

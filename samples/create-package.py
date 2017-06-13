940be0433499b47b32a07e56a55fe8ffece9b609
Max Barrass <max.barrass@gmail.com>
2017-06-14 07:46:37 +1000

    fix syntax to support py36

--- samples/create-package.py
+++ samples/create-package.py
@@ -11,31 +11,31 @@ try:
 
     # check result status
     if result.is_success():
-        print 'Success: {0}'.format(result.message)
+        print('Success: {0}'.format(result.message))
     else:
-        print 'Failure: {0}'.format(result.message)
+        print('Failure: {0}'.format(result.message))
 
     # debug response and request details via result
-    print result.response['http_code']
-    print result.response['body']
-    print result.response['request']['method']
-    print result.response['request']['url']
-    print result.response['request']['params']
+    print(result.response['http_code'])
+    print(result.response['body'])
+    print(result.response['request']['method'])
+    print(result.response['request']['url'])
+    print(result.response['request']['params'])
 
     # debug all response and request details in a single string
-    print result.debug()
+    print(result.debug())
 
-except pyaem.PyAemException, e:
+except pyaem.PyAemException as e:
 
     # exception message
-    print e.message
+    print(e.message)
 
     # exception code uses response http_code
-    print e.code
+    print(e.code)
 
     # debug response and request details via exception
-    print e.response['http_code']
-    print e.response['body']
-    print e.response['request']['method']
-    print e.response['request']['url']
-    print e.response['request']['params']
+    print(e.response['http_code'])
+    print(e.response['body'])
+    print(e.response['request']['method'])
+    print(e.response['request']['url'])
+    print(e.response['request']['params'])
